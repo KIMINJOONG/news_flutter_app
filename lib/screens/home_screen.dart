ea6fa1ca180cfe5ab5f1a9184e2d6e71f55c1edc
kiminjoong <ijboym216@gmail.com>
2022-11-27 13:25:42 +0900

    12. Finish the tabs implementation

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,8 +3,10 @@ import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_iconly/flutter_iconly.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:news_app_flutter_course/consts/vars.dart';
 import 'package:news_app_flutter_course/services/utils.dart';
 import 'package:news_app_flutter_course/widgets/drawer_widget.dart';
+import 'package:news_app_flutter_course/widgets/tabs.dart';
 import 'package:provider/provider.dart';
 
 import '../providers/theme_provider.dart';
@@ -17,6 +19,8 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  var newsType = NewsType.allNews;
+
   @override
   Widget build(BuildContext context) {
     final Color color = Utils(context).getColor;
@@ -52,23 +56,38 @@ class _HomeScreenState extends State<HomeScreen> {
               padding: const EdgeInsets.all(8.0),
               child: Row(
                 children: [
-                  GestureDetector(
-                    onTap: () {},
-                    child: Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(15),
-                        color: Theme.of(context).cardColor,
-                      ),
-                      child: const Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: const Text(
-                          'All news',
-                          style: TextStyle(
-                            fontSize: 22,
-                          ),
-                        ),
-                      ),
-                    ),
+                  TabsWidget(
+                    text: 'All News',
+                    color: newsType == NewsType.allNews
+                        ? Theme.of(context).cardColor
+                        : Colors.transparent,
+                    function: () {
+                      if (newsType == NewsType.allNews) {
+                        return;
+                      }
+                      setState(() {
+                        newsType = NewsType.allNews;
+                      });
+                    },
+                    fontSize: newsType == NewsType.allNews ? 22 : 14,
+                  ),
+                  SizedBox(
+                    width: 25,
+                  ),
+                  TabsWidget(
+                    text: 'All News',
+                    color: newsType == NewsType.topTrending
+                        ? Theme.of(context).cardColor
+                        : Colors.transparent,
+                    function: () {
+                      if (newsType == NewsType.topTrending) {
+                        return;
+                      }
+                      setState(() {
+                        newsType = NewsType.topTrending;
+                      });
+                    },
+                    fontSize: newsType == NewsType.topTrending ? 22 : 14,
                   ),
                 ],
               ),

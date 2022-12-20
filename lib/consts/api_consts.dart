3e2668dd6a54f83fd38b64a78f13857b0ffa5d63
kiminjoong <ijboym216@gmail.com>
2022-12-21 08:40:02 +0900

    1. Enhance the get all news method with url segmentation

---
+++ lib/consts/api_consts.dart
@@ -0,0 +1,4 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+String BASEURL = "newsapi.org";
+String? API_KEY = dotenv.env['API_KEY'];
--- lib/services/news_api.dart
+++ lib/services/news_api.dart
@@ -4,10 +4,20 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
 import 'package:news_app_flutter_course/models/news_model.dart';
 
+import '../consts/api_consts.dart';
+
 class NewsAPiServices {
   Future<List<NewsModel>> getAllNews() async {
-    var url = Uri.parse('https://newsapi.org/v2/everything?q=bitcoin&pageSize=5&apiKey=${dotenv.env['API_KEY']}');
-    var response = await http.get(url);
+    // var url = Uri.parse('https://newsapi.org/v2/everything?q=bitcoin&pageSize=5&apiKey=${dotenv.env['API_KEY']}');
+
+    var uri = Uri.https(BASEURL, "v2/everything", {
+      "q": "bitcoin",
+      "pageSize": "5",
+      "apiKey": API_KEY,
+    });
+    var response = await http.get(uri, headers: {
+      "x-Api-key": API_KEY!
+    });
     Map data = jsonDecode(response.body);
     List newsTempList = [];
     for (var v in data['articles']) {
